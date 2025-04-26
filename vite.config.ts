a394f349780f85e2b67ad07d0d599c9f2cf095e3
panupong.ta <panucsvip29@gmail.com>
2025-04-27 05:44:14 +0700

    feat: build new

--- vite.config.ts
+++ vite.config.ts
@@ -14,6 +14,7 @@ export default defineConfig({
         react(),
         tailwindcss(),
     ],
+    base: process.env.APP_ENV === 'production' ? '/build/' : '/',
     esbuild: {
         jsx: 'automatic',
     },
